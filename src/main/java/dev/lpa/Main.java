816dac8d26fdba1854b30525fdba36ca562e8f5d
Aurélien Lonni <aurelien.lonni@gmail.com>
2023-07-25 10:36:08 +0200

    Challenge part 2
    Creation of the equals, hashCode, addEmail and replaceEmailIfExists methods in the Contact class.

--- src/main/java/dev/lpa/Main.java
+++ src/main/java/dev/lpa/Main.java
@@ -1,7 +1,9 @@
 package dev.lpa;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Main {
 
@@ -33,6 +35,39 @@ public class Main {
 //        Robin Hood: [rhood@gmail.Com] []
 //        Linus Van Pelt: [lvpelt2015@gmail.com] []
 //        Daffy Duck: [daffy@google.com] []
+
+        Set<Contact> emailContacts = new HashSet<>(emails);
+        Set<Contact> phoneContacts = new HashSet<>(phones);
+        printData("Phone contacts", phoneContacts);
+//        --------------------
+//        Phone contacts
+//        -------------------
+//        Charlie Brown: [] [(333) 444-5555]
+//        Maid Marion: [] [(123) 456-7890]
+//        Robin Hood: [] [(564) 789-3000]
+//        Mickey Mouse: [] [(999) 888-7777]
+//        Minnie Mouse: [] [(456) 780-5666]
+
+        printData("Email contacts", emailContacts);
+//        --------------------
+//        Email contacts
+//        --------------------
+//        Linus Van Pelt: [lvpelt2015@gmail.com] []
+//        Robin Hood: [rhood@gmail.Com] []
+//        Mickey Mouse: [mckmouse@gmail.com] []
+//        Daffy Duck: [daffy@google.com] []
+//        Minnie Mouse: [minnie@verizon.net] []*
+
+        int index = emails.indexOf(new Contact("Robin Hood"));
+        Contact robinHood = emails.get(index);
+        robinHood.addEmail("Sherwood Forest");
+//        Robin Hood now has email RHood@sherwoodforest.com
+        robinHood.addEmail("Sherwood Forest");
+//        Robin Hood already has email RHood@sherwoodforest.com
+        robinHood.replaceEmailIfExists("RHood@sherwoodforest.com", "RHood@sherwoodforest.org");
+        System.out.println(robinHood);
+//        Robin Hood: [RHood@sherwoodforest.org, rhood@gmail.Com] []
+
     }
 
     public static void printData(String header, Collection<Contact> contacts) {
